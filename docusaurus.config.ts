325acdf30db35d171bc38357dc81a29618b59d43
Johnathan Sewell <286782+johnathan-sewell@users.noreply.github.com>
2024-11-02 10:58:33 +0100

    feat(favicon): update + prettier

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,55 +1,56 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "Johnathan Sewell",
   tagline: "Developer Journal",
-  
-  favicon: 'img/favicon.ico',
+
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
   url: "https://johnathan-sewell.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "johnathan-sewell", // Usually your GitHub org/user name.
   projectName: "johnathan-sewell.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         googleAnalytics: {
-          trackingID: "G-WYHQ8V99F6"
+          trackingID: "G-WYHQ8V99F6",
         },
         docs: false,
         // https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-content-blog#ex-config
         blog: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           showReadingTime: true,
-          blogTitle: 'Developer Journal',
-          blogDescription: 'A blog about software development and other things.',
-          postsPerPage: 'ALL',
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
+          blogTitle: "Developer Journal",
+          blogDescription:
+            "A blog about software development and other things.",
+          postsPerPage: "ALL",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -57,8 +58,8 @@ const config: Config = {
 
   themeConfig: {
     navbar: {
-      title: 'Johnathan Sewell',
-      style: 'dark',
+      title: "Johnathan Sewell",
+      style: "dark",
     },
   } satisfies Preset.ThemeConfig,
 };
